7f15e359fc9594619459b8204f8fe8aeb9b78660
Helen Hood <helen.c.hood@gmail.com>
2014-09-05 15:43:26 -0400

    Create array of Account objects

--- account.rb
+++ account.rb
@@ -1,4 +1,9 @@
 class Account
+  def initialize(account_data)
+    @name = account_data['Account']
+    @starting_balance = account_data['Balance']
+  end
+
   def starting_balance
   end
 
--- accounts_summary.rb
+++ accounts_summary.rb
@@ -1,4 +1,17 @@
+require 'csv'
+require 'pry'
+
+require_relative 'account'
+
 # TODO:
 # - Create our Account objects & our Transaction objects,
 # and associate the two
 # - Output a summary for each Account object
+
+# create an array of Account objects
+accounts = []
+
+CSV.foreach('balances.csv', headers: true, converters: :numeric) do |row|
+  accounts << Account.new(row)
+end
+
