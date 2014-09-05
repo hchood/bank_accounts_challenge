a04ad32c96020971be69d0dba16c776cd2e79a57
Helen Hood <helen.c.hood@gmail.com>
2014-09-05 15:47:47 -0400

    Create array of Transaction objects

--- accounts_summary.rb
+++ accounts_summary.rb
@@ -2,6 +2,7 @@ require 'csv'
 require 'pry'
 
 require_relative 'account'
+require_relative 'transaction'
 
 # TODO:
 # - Create our Account objects & our Transaction objects,
@@ -14,4 +15,18 @@ accounts = []
 CSV.foreach('balances.csv', headers: true, converters: :numeric) do |row|
   accounts << Account.new(row)
 end
+
+# create an array of Transaction objects
+transactions = []
+
+CSV.foreach('bank_data.csv', headers: true, converters: :numeric) do |row|
+  transactions << Transaction.new(row)
+end
+
+binding.pry
+
+
+
+
+
 
--- transaction.rb
+++ transaction.rb
@@ -1,4 +1,11 @@
 class Transaction
+  def initialize(transaction_data)
+    @date = transaction_data['Date']
+    @amount = transaction_data['Amount']
+    @description = transaction_data['Description']
+    @account = transaction_data['Account']
+  end
+
   def deposit?
   end
 
